0a8d4e792ff4323b0fec9b18058afe07fa312202
infowaydevteam <infowaydevteam@gmail.com>
2025-03-20 08:34:07 -0700

    Add files via upload

---
+++ src/Components/About/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './About.css';
+
+const About = () => (
+  <section className="about-section" id="about">
+    <div className="about-container">
+      <h2>About Dr. Eva-Marie Muller-Stuler</h2>
+      <h3>Bio</h3>
+      <p>
+        Dr. Eva-Marie Muller-Stuler is leading the Data &amp; AI practice for Ernst &amp; Young for the Middle East and North Africa. She is responsible for the development of Data Governance frameworks, Data Strategies and the implementation of complex data science and AI projects and transformations.
+      </p>
+
+    </div>
+  </section>
+);
+
+export default About;
